306eab61922bfbff4bb7db686f9e67ef1c55b4e6
leeli <oolzpishere@126.com>
2021-02-24 23:16:57 +0800

    update webpack config

---
+++ config/webpack/custom.js
@@ -0,0 +1,18 @@
+
+
+module.exports = {
+  resolve: {
+    extensions: ['.css'],
+    alias: {
+      jquery: 'jquery/src/jquery',
+      Turbolinks: "turbolinks",
+      Popper: ['popper.js', 'default'],
+
+      // vue: 'vue/dist/vue.js',
+      // React: 'react',
+      // ReactDOM: 'react-dom',
+      // vue_resource: 'vue-resource/dist/vue-resource'
+    }
+  }
+
+}
